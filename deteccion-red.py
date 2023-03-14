aa97c292b4c749c29697687f9718f4fbf1fbef85
RodrigoVilla <73896948+RodrigoAvalosVilla@users.noreply.github.com>
2023-03-13 23:32:51 -0600

    Ya se puede mapear la direccion

--- deteccion-red.py
+++ deteccion-red.py
@@ -1,7 +1,7 @@
-# 1) Pedir Host
-# 2) Detectar host
-# 3) Mapear red
-# 4) Guardar red en bdd
+# 1) Pedir Host - ya
+# 2) Detectar host - ya
+# 3) Mapear red - ya
+# 4) Guardar red en bdd - en proceso
 # 5) Mostrar bdd
 # 6) Obtener pagina web
 
@@ -9,26 +9,68 @@
 
 # ------------- LIBRERIAS ---------------
 import os
+import nmap
+import socket
+import pymongo
+from pymongo import MongoClient
 
+#
 
 # ------------- FUNCIONES ---------------
+
+# Funcion para realizar un ping de un enlace
 def hacerPing(ruta):
     return os.system("ping -n 1 " + ruta)
 
-# Cosas por hacer al método de mapeo:
-# 1) Obtener el resultado del comando para manejar su contenido
-# 2) Retornar el resultado en un diccionario de datos.
+# Funcion que retorna los puertos y su estado en forma de diccionario de datos
 def mapeoPuertos(ruta):
-    comando = "nmap " + str(ruta)
+    print("Mapeando la ruta: ", ruta, " ...")
+    ip = socket.gethostbyname(ruta)
 
-    return os.system(comando)
+    nm = nmap.PortScanner()
 
+    print("Ip: ", ip)
+
+    nm.scan(ip, arguments='-p-')
+
+    print("State: ", nm[ip].state())
+
+    diccionario = {"Enlace": ruta}
+
+    for host in nm.all_hosts():
+        print('Puertos abiertos en', host)
+        for port in nm[host]['tcp'].keys():
+            estado = nm[host]['tcp'][port]['state']
+            diccionario.update({port: estado})
+    
+    return diccionario
+
+# Funcion para insertar lo obtenido al mapear el enlace
+def insertarDB(datos):
+    cliente = MongoClient()
+    db = cliente['red']
+    coleccion = db['puertos']
+    resultado = coleccion.insert_one(datos)
+
+    return resultado.acknowledged
+
+# Funcion para mostrar el contenido de la BDD
+def mostrarDB():
+    cliente = MongoClient()
+    db = cliente['red']
+    coleccion = db['puertos']
 # ------------- CODIGO ---------------
 ruta = input("Dame la IP o nombre del dominio: ")
 
 if hacerPing(ruta) == 0:
     # Si al hacer ping da 0, mapeamos la ruta
-    print("RESULTADO: \n", mapeoPuertos(ruta))
+    resultadoMapeo = mapeoPuertos(ruta)
+    # Una vez mapeado, lo insertamos a la bdd de mongo
+    resultadoInsert = insertarDB(resultadoMapeo)
+    if resultadoInsert:
+        mostrarDB()
+    else:
+        print("No se inserto correctamente")
 else:
     # Sino la ruta no es accesible y fin del programa
     print("Dominio o IP no valido...")
